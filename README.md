401c4467eab4db9694c0f95e12dd2d7d6ca5dc42
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-03-05 19:11:43 +0000

    passed

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 
 2. Create a fucntion called _createArrayList()_. It takes in an Array of doubles and returns an ArrayList of the same doubles.
 
-3. Create a function called _combineParallelArrays()_.  It takes in two arrays that are parallel as parameters, a String array of names and a integer array of ages.
-   Combine the two together in a hashmap with the names as keys and ages as definitions.  Return the HashMap.
+3. 
 
 4. Create a function called _increaseAge()_.  It takes in a HashMap(String, Integer) and a String (name) as parameters.  Increase the age (values) associated with the name by one.
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,8 +2,62 @@ import java.util.*;
 
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void swap(ArrayList<String> arr, int x, int y) {
+		if (x >= 0 && x < arr.size() && y >= 0 && y < arr.size()) {
+			String z = arr.get(x);
+			arr.set(x, arr.get(y));
+			arr.set(y, z);
+		}
+	}
 
+	public static ArrayList<Double> createArrayList(double[] ar) {
+		ArrayList<Double> arr = new ArrayList<>();
+		for (double num : ar) {
+			arr.add(num);
+		}
+		return arr;
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages) {
+		HashMap<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < names.length; i++) {
+			map.put(names[i], ages[i]);
+		}
+		return map;
+	}
+
+	public static void increaseAge(HashMap<String, Integer> map, String name) {
+		if (map.containsKey(name)) {
+			map.put(name, map.get(name) + 1);
+		}
+	}
+
+	public static void main(String args[]) {
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("Dream");
+		arr.add("Clean");
+		arr.add("Drive");
+		arr.add("Tree");
+		arr.add("Potato");
+
+		swap(arr, 2, 4);
+		System.out.println(arr);
+
+		double[] ar = {2.3, 4.2, 1.2, 5.3, 3.0};
+		ArrayList<Double> list = createArrayList(ar);
+		System.out.println(list);
+
+		String[] names = {"John", "Joane", "Kelsey"};
+		int[] ages = {20, 25, 26};
+		HashMap<String, Integer> map = combineParallelArrays(names, ages);
+		System.out.println(map);
+
+		HashMap<String, Integer> maps = new HashMap<>();
+		maps.put("John", 40 + 1);
+		maps.put("Dwayne", 46 + 1);
+		maps.put("Kevin", 43 + 1);
+
+		System.out.println(maps);
 	}
 
 }
